4e622dbc27ad9085de81ad26207a395f3b37b6da
ishanrana363 <ishanrana363@gmail.com>
2025-03-29 12:12:45 +0600

    input verify page design completed

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EmailFrom from './emailFrom'
+
+const page = () => {
+  return (
+    <>
+    <EmailFrom></EmailFrom>
+    </>
+  )
+}
+
+export default page
---
+++ src/app/(auth)/otp-verify/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import OtpFrom from './otpFrom'
+
+const Page = () => {
+  return (
+    <div>
+        <OtpFrom></OtpFrom>
+    </div>
+  )
+}
+
+export default Page
