15aba7d6c62a3b2c6b3f61c6a9a002deb0b41dc7
Ksh Siaan <kshsiaan@gmail.com>
2025-04-16 09:29:41 +0600

    finalized

--- src/app/(view)/shop/[category]/page.tsx
+++ src/app/(view)/shop/[category]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ShopPage from "./shop-page";
+import { Allproducts } from "@/lib/products";
 
 export interface Product {
   id: number;
@@ -7,57 +8,6 @@ export interface Product {
   name: string;
   price: number;
 }
-
-const Allproducts: Product[] = [
-  {
-    id: 1,
-    image: "/product1.png",
-    name: "Milly & Rio Hoodies",
-    price: 4820.0,
-  },
-  {
-    id: 2,
-    image: "/product/product2.png",
-    name: "Shocks for children",
-    price: 6150.0,
-  },
-  {
-    id: 3,
-    image: "/product/product3.jpg",
-    name: "SmartSphere",
-    price: 3990.0,
-  },
-  {
-    id: 4,
-    image: "/product/product4.png",
-    name: "InnovaGear",
-    price: 7500.0,
-  },
-  {
-    id: 5,
-    image: "/product/product5.png",
-    name: "T-shirts for children",
-    price: 9100.0,
-  },
-  {
-    id: 6,
-    image: "/product/product6.png",
-    name: "Visionary Hub",
-    price: 5400.0,
-  },
-  {
-    id: 7,
-    image: "/product/product7.jpg",
-    name: "QuantumLeap",
-    price: 8300.0,
-  },
-  {
-    id: 8,
-    image: "/product/product8.jpg",
-    name: "EcoFusi2675",
-    price: 2675.0,
-  },
-];
 
 const Page = ({ params }: { params: { category: string } }) => {
   console.log(params.category);
